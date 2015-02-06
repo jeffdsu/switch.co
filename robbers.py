4974656420bb624917acc655ca7ef7b6aa27f85f
jeffdsu <jeffdsu@gmail.com>
2015-02-06 14:25:51 -0800

    new way to do it.

--- robbers.py
+++ robbers.py
@@ -2,68 +2,80 @@
 # -*- coding: utf-8 -*-
 
 
+def getHitList (valuables) : 
+	"""Returns the list of hits that should be taken"""
+	hitlist = []
+	maxHitCount = len(a) / 2 if len(a) % 2 == 0 else (len(a)+1)/2
+	hitworthPerHouse = [0] * len(valuables)
 
-def findOptimalRobbingHitandHitIt (valuables) :
-	"""Returns the most optimal hit for robbers to take,  returns 0 when no more work can be done."""
-
-
-	#-- first find how much each hit is worth.  this includes the money lost from the neigboaring houses
-	maxvalue = -1
-	maxindex = 0
-	#hitworthPerHouse = [0] * len(valuables)
-	currentHitWorth = -1
 	for i in xrange ( 0, len(valuables) ) :
 		if valuables[i] != 'x' :
-			currentHitWorth = valuables[i]
-			currentHitWorth = currentHitWorth - valuables[i+1] if i < (len(valuables) - 1) and valuables[i+1] != 'x' else currentHitWorth
-			currentHitWorth = currentHitWorth - valuables[i-1] if i > 0 and valuables[i-1] !='x' else currentHitWorth
+			hitworthPerHouse [i] = valuables[i]
+			hitworthPerHouse [i] = hitworthPerHouse [i] - valuables[i+1] if i < (len(valuables) - 1)  else hitworthPerHouse [i]
+			hitworthPerHouse [i] = hitworthPerHouse [i] - valuables[i-1] if i > 0 else hitworthPerHouse [i]
 
+		
+	for i in xrange (0, maxHitCount) :
+		print hitworthPerHouse
+		hit = searchForBestHit(hitworthPerHouse)
+		hitlist.append (hit)
+		hitHouseUpdateHitWorth(hitworthPerHouse, valuables, hit)
 
-			#-- keep track of the maxvalue so I can hit the house afterwards
-			if i == 0 :
-				maxvalue = currentHitWorth
-			elif maxvalue < currentHitWorth :
-				maxvalue = currentHitWorth
+	return hitlist
+def comparewithX (a) :
+	if a == 'x' : 
+		print a
+		return 
+def searchForBestHit (hitworthPerHouse) :
+	"""Gets index of best hit"""
+	#return hitworthPerHouse.index(max(hitworthPerHouse, cmp=comparewithX))
+	maxindex = -1
+	foundint = False
+	for i in xrange ( 0, len(hitworthPerHouse) ) :
+		if hitworthPerHouse[i] != 'x':
+			if foundint == False:
+				maxvalue = hitworthPerHouse [i]
 				maxindex = i
+				foundint = True
+			elif foundint == True and  maxvalue < hitworthPerHouse [i] :
+				maxvalue = hitworthPerHouse [i]
+				maxindex = i
+	return maxindex
 
 
-	if maxindex >= 0:
-		#hit the house
-		hitValue = hitHouseAndSendTheCops (valuables, maxindex)
-		if hitValue > 0 :
-			return hitValue
-	return 0
-	
+def hitHouseUpdateHitWorth (hitworthPerHouse, valuables, houseIndex) :
+	"""Updates the hit worth after cops come"""
+	if houseIndex < 0 or houseIndex >= len(valuables) : 
+		return 
+	hitworthPerHouse [houseIndex] = 'x'
+	if (houseIndex + 1) < len(valuables) :
+		hitworthPerHouse [houseIndex+1] = 'x'
+	if (houseIndex - 1) >= 0 :
+		hitworthPerHouse [houseIndex-1] = 'x'
+	if (houseIndex+2) < len(valuables) and hitworthPerHouse [houseIndex+2] != 'x':
+		hitworthPerHouse [houseIndex+2] = hitworthPerHouse [houseIndex+2] + valuables [houseIndex+1]
+	if (houseIndex-2) >=0 and hitworthPerHouse [houseIndex-2] != 'x':
+		hitworthPerHouse [houseIndex-2] = hitworthPerHouse [houseIndex-2] + valuables [houseIndex-1]
 
-
-def hitHouseAndSendTheCops (valuables, houseIndex) :
-	"""Returns value of hit while sending the cops to clear the neighboring houeses"""
-	t = valuables [houseIndex]
-	valuables [houseIndex] = 'x'
-	if houseIndex > 0 :
-		valuables [houseIndex - 1] = 'x'
-	if houseIndex < (len(valuables) - 1) :
-		 valuables [houseIndex + 1] = 'x'
-	if t == 'x':
-		return 0
-	return t
-#def findBestValueDumbWay (valuables) :
-#	for i in xrange ( 0, len(valuables) ) :
-
-
-
+def getBestHitTotal (valuables) :
+	"""Returns the sum up the hit total value"""
+	hitlist = getHitList(valuables)
+	print hitlist
+	totalHitWorth = 0
+	for h in hitlist :
+		if h >= 0 :
+			totalHitWorth = totalHitWorth + a[h]
+	return totalHitWorth
+ 
 totalHitWorth = 0
 a = [20, 10, 50, 5, 1]
-a = [20, 50, 10, 1, 5]
-a = [30, 50, 20, 1]
-a = [30, 50, 20, 1, 2]
-a = [30, 50, 50, 1, 2]
-a = [30, 50, 50, 1, 2, 50, 50, 50]
-a = [20, 50, 30, 20, 50, 20]
-#no matter what, the max amount houses that can be hit is len / 2
-for i in xrange(0, len(a) / 2 if len(a) % 2 == 0 else (len(a)+1)/2 ) :
-	totalHitWorth = totalHitWorth + findOptimalRobbingHitandHitIt(a)
+#a = [20, 50, 10, 1, 5]
+#a = [30, 50, 20, 1]
+#a = [30, 50, 20, 1, 2]
+#a = [30, 50, 50, 1, 2]
+#a = [30, 50, 50, 1, 2, 50, 50, 50]
+#a = [20, 50, 20, 20, 50, 20]
 
-print "Best Hit : " + str(totalHitWorth)
+print "Best Hit : " + str(getBestHitTotal(a))
 
 
